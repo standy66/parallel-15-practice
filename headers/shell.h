925061f5b9b25b8035696f3d0199c427c4ccd098
Andrew Stepanov <karn9050@gmail.com>
2015-09-19 18:23:31 +0300

    Implement shell response to actions

--- headers/shell.h
+++ headers/shell.h
@@ -2,8 +2,18 @@
 
 #include <istream>
 #include <ostream>
+#include <map>
 
 class Shell {
 public:
+  typedef int (*Action) (std::istream& argument_stream, std::ostream& out,
+                         std::ostream& err);
+
+  Shell(std::map<std::string, Action> actionMap);
+  Shell();
+
   int run(std::istream& in, std::ostream& out, std::ostream& err);
+
+private:
+  std::map<std::string, Action> actionMap;
 };
--- src/shell.cpp
+++ src/shell.cpp
@@ -1,15 +1,33 @@
 #include "shell.h"
 
 #include <string>
+#include <sstream>
 
 #include "dbg.h"
+
+Shell::Shell() : actionMap() { }
+
+Shell::Shell(std::map<std::string, Shell::Action> map)
+  : actionMap(map) { }
 
 int Shell::run(std::istream& in, std::ostream& out, std::ostream& err) {
   std::string line;
   while (std::getline(in, line)) {
-    DBG("Got line: " << line);
-
-
+    //DBG("Got line: " << line);
+    std::stringstream linestream;
+    linestream << line;
+    std::string commandName;
+    linestream >> commandName;
+    //DBG("Command name: " << commandName);
+    Action action = actionMap[commandName];
+    if (action != 0) {
+      int resultCode = action(linestream, out, err);
+      if (resultCode != 0) {
+        err << "Command " << commandName << " exited with code " << resultCode << std::endl;
+      }
+    } else {
+      err << "Command " << commandName << " not found" << std::endl;
+    }
   }
   return 0;
 }
