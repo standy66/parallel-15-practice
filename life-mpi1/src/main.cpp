003bbe9a9f8aced89b949c610c31818cc2234c23
Andrew Stepanov <karn9050@gmail.com>
2015-11-23 22:50:46 +0300

    Fix CMakeLists.txt

--- life-mpi1/src/main.cpp
+++ life-mpi1/src/main.cpp
@@ -1,18 +1,24 @@
-//#include <mpi.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+void master() {
+
+}
+
+void slave() {
+
+}
+
 int main(int argc, char** argv) {
-/*  MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   printf("rank: %d, size: %d\n", rank, size);
   MPI_Finalize();
-  scanf("%d", &rank, &size);
-  return 0;*/
-
+  return 0;
 }
