55a86e8cf17840615f48576cee35de8a9e994e0b
Andrew Stepanov <karn9050@gmail.com>
2015-11-23 22:33:15 +0300

    Add life-mpi1 to cmake build system

--- life-mpi1/src/main.cpp
+++ life-mpi1/src/main.cpp
@@ -1,13 +1,18 @@
-#include <mpi.h>
+//#include <mpi.h>
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char** argv) {
-  MPI_Init(&argc, &argv);
+/*  MPI_Init(&argc, &argv);
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   printf("rank: %d, size: %d\n", rank, size);
   MPI_Finalize();
   scanf("%d", &rank, &size);
-  return 0;
+  return 0;*/
+
 }
